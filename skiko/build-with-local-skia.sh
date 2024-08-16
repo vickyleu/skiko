6c81b7cbce218c71bfe67dbe05d770c360148ce0
vicky leu <yu6564172@gmail.com>
2024-08-16 20:38:02 +0800

    m121

--- skiko/build-with-local-skia.sh
+++ skiko/build-with-local-skia.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ####### Variables you can edit to change build config, or set same environment variables before script execution #######
-#SKIA_VERSION="${SKIA_VERSION:="m121"}" # Version of Skia m###-commit-sha-#. This commit sha will be cloned from repository https://github.com/JetBrains/skia
-SKIA_VERSION="${SKIA_VERSION:="m116-47d3027-1"}" # Version of Skia m###-commit-sha-#. This commit sha will be cloned from repository https://github.com/JetBrains/skia
+SKIA_VERSION="${SKIA_VERSION:="m121"}" # Version of Skia m###-commit-sha-#. This commit sha will be cloned from repository https://github.com/JetBrains/skia
+#SKIA_VERSION="${SKIA_VERSION:="m116-47d3027-1"}" # Version of Skia m###-commit-sha-#. This commit sha will be cloned from repository https://github.com/JetBrains/skia
 SKIA_DEBUG_MODE="${SKIA_DEBUG_MODE:="false"}" # in debug mode Skiko will be published with postix "+debug", for example "0.0.0-SNAPSHOT+debug"
 SKIA_TARGET="${SKIA_TARGET:="ios"}" # possible values: "ios", "iosSim", "macos", "windows", "linux", "wasm", "android", "tvos", "tvosSim"
 #SKIA_TARGET="${SKIA_TARGET:="iosSim"}" # possible values: "ios", "iosSim", "macos", "windows", "linux", "wasm", "android", "tvos", "tvosSim"
@@ -79,8 +79,9 @@ SCRIPT_DIR="$(pwd)"
 
 #git clone https://github.com/JetBrains/skia-pack.git || echo "skia-pack exists. You can remove it or update by hands with git pull"
 cd skia-pack
-#[ -d "skia" ] && echo "skip cript/checkout.py, because directory skia-pack/skia already exists"
-#[ ! -d "skia" ] && python3 script/checkout.py --version "$SKIA_VERSION"
+[ -d "skia" ] && echo "skip script/checkout.py, because directory skia-pack/skia already exists"
+[ ! -d "skia" ] && python3 script/checkout.py --version "$SKIA_VERSION"
+# shellcheck disable=SC2068
 for skikoMachine in ${skikoMachines[@]}; do
   python3 script/build.py --target "$SKIA_TARGET" --machine "$skikoMachine" --build-type "$skikoBuildType"
   python3 script/archive.py --version "$SKIA_VERSION" --target "$SKIA_TARGET" --machine "$skikoMachine" --build-type "$skikoBuildType"
